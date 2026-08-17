8a98b0d9829ca7b5aed1a4f1b6c55a6ecba93d27
libo-liti <000525jh@gmail.com>
2026-08-17 11:22:44 +0900

    feat: 그리디 문제 풀이 (조이스틱, 구명보트)

--- programmers_level2/42860.cpp
+++ programmers_level2/42860.cpp
@@ -1,10 +1,34 @@
 #include <string>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(string name)
 {
     int answer = 0;
+
+    // 세로 이동
+    for (int i = 0; i < name.size(); i++)
+    {
+        int down = name[i] - 'A';
+        int up = 'Z' - name[i] + 1;
+        answer += min(down, up);
+    }
+
+    // 가로 이동
+    int width = name.size() - 1;
+    for (int i = 0; i < name.size(); i++)
+    {
+        int next = i + 1;
+        while (next < name.size() && name[next] == 'A')
+            next++;
+
+        int back = i * 2 + (name.size() - next);
+        int back2 = (name.size() - next) * 2 + i;
+
+        width = min(width, min(back, back2));
+    }
+    answer += width;
+
     return answer;
 }
